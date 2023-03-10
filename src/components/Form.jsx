dc9b9f7788485940295babd21312b216cead5064
Yemisirach <yemsrach3723@gmail.com>
2023-03-10 13:34:21 +0300

    Initialize project with components

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder="Book Title" />
+        <input type="text" placeholder="Author" />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/pages/Books.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Book from '../components/Book';
+import Form from '../components/Form';
+
+function Books() {
+  return (
+    <div>
+      <Book title="Dead Aid" author="Dr.Dambisa mayo" />
+      <Form />
+    </div>
+  );
+}
+
+export default Books;
