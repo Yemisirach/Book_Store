1f3afa04772127df02538630d1318eb181135647
Yemisirach <yemsrach3723@gmail.com>
2023-03-14 13:50:08 +0300

    Import catagories slice and rename unmached files

--- src/redux/Books/bookSlice.jsx
+++ src/redux/Books/bookSlice.jsx
@@ -1,26 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  bookItems: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
 };
 
 const bookSlice = createSlice({
@@ -30,11 +11,11 @@ const bookSlice = createSlice({
   reducers: {
     addBook: (state, action) => ({
       ...state,
-      book: [...state, action.payload],
+      books: [...state, action.payload],
     }),
     removeBook: (state, action) => ({
       ...state,
-      book: state.books.filter((book) => book.id !== action.payload),
+      books: state.books.filter((book) => book.id !== action.payload),
     }),
   },
 });
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './Books/bookSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     book: cartReducer,
+    categories: categoriesReducer,
   },
 });
 
